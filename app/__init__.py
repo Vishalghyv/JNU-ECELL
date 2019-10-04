e1a3cd724f909c91e69ac73601a631076df87039
Vishalghyv <vishal38785@gmail.com>
2019-10-04 14:29:01 +0530

    fd

--- app/__init__.py
+++ app/__init__.py
@@ -1,2 +1,12 @@
 from flask import Flask,render_template,request,session
 app = Flask(__name__ ,template_folder='../frontend/html',static_folder='../frontend')
+@app.route('/')
+def home():
+	print("Hello")
+	return render_template("ecell.html")
+@app.route('/sponser')
+def sponser():
+	return render_template("sponser.html")
+
+if __name__ == "__main__":
+	app.run()
