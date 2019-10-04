ad25fdc6aa6f6f1634c93a397e798154d70a2283
Vishalghyv <vishal38785@gmail.com>
2019-10-04 12:24:10 +0530

    df

--- script.py
+++ script.py
@@ -2,11 +2,12 @@ from flask import Flask,render_template,request,session
 from flask import Flask, request, redirect, url_for
 
 
-app = Flask(__name__ ,template_folder='/FrontEnd/Html',static_folder='/FrontEnd')
+app = Flask(__name__ ,template_folder='./frontend/html',static_folder='./frontend')
 app.secret_key = "y337kGcys"
 
 @app.route('/')
 def home():
+	print("Hello")
 	return render_template("ecell.html")
 @app.route('/sponser')
 def sponser():
