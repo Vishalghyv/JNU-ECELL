d8bc8438686636b3d0a9f30a8b5c2d3eff634f5c
Vishalghyv <vishal38785@gmail.com>
2019-10-04 13:20:39 +0530

    fdf

--- script.py
+++ script.py
@@ -3,7 +3,6 @@ from flask import Flask, request, redirect, url_for
 
 
 app = Flask(__name__ ,template_folder='./frontend/html',static_folder='./frontend')
-app.secret_key = "y337kGcys"
 
 @app.route('/')
 def home():
