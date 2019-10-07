e62e6c2176a4bb9bbd620f02a590e08a2f6dfcd3
Vishalghyv <vishal38785@gmail.com>
2019-10-07 19:10:05 +0530

    hei

--- app/app.py
+++ app/app.py
@@ -10,7 +10,8 @@ posts ={
 		"Having a deep passion and or drive to complete something from start to finish is a common trait in many entrepreneurs. If you find yourself staying awake at night because you think you can fix something even better, or if you daydream on ways to improve something, you may be an entrepreneur without realizing it."
 		],
 		"heading":"Start Up",
-		"img":"resources/img1.jpg"
+		"img":"resources/img1.jpg",
+		"register":0
 		},
 	"post2":{"article":[
 		"Almost everyone, if given a chance would prefer the position a job giver instead of job seeker. Entrepreneurship gives u a chance to be a job giver. It’s a way to give it back to the society through the services you create. The precise definition of entrepreneurship is the process of creating new business or market. An entrepreneur usually starts a business form complete scratch , but one can also buy an old company n rebuild it. According to the job-listing site, Monster, there are nine characteristics of entrepreneurs and the entrepreneurial journey. They include motivation, creativity, hands-on, versatility, business skills, drive, vision, flexibility, and decisiveness.",
@@ -19,7 +20,9 @@ posts ={
 		"Having a deep passion and or drive to complete something from start to finish is a common trait in many entrepreneurs. If you find yourself staying awake at night because you think you can fix something even better, or if you daydream on ways to improve something, you may be an entrepreneur without realizing it."
 		],
 	"heading":"E-Submit",
-	"img":"resources/img2.jpg"},
+	"img":"resources/img2.jpg",
+	"register":1
+	},
 	"post3":{"article":[
 		"Almost everyone, if given a chance would prefer the position a job giver instead of job seeker. Entrepreneurship gives u a chance to be a job giver. It’s a way to give it back to the society through the services you create. The precise definition of entrepreneurship is the process of creating new business or market. An entrepreneur usually starts a business form complete scratch , but one can also buy an old company n rebuild it. According to the job-listing site, Monster, there are nine characteristics of entrepreneurs and the entrepreneurial journey. They include motivation, creativity, hands-on, versatility, business skills, drive, vision, flexibility, and decisiveness.",
 		"Generating an entrepreneurial mindset can improve how you think about business opportunities whether it’s for a small or large business, family-owned or venture-backed, or a social media entrepreneurship venture. Entrepreneurship training helps expose you to fundamental concepts and analytical tools such as the lean startup process to help improve your chance for success. Here at JNU we  work in hand in hand with you , matching shoulder to shoulder giving wings to your ideas and realization to your dreams.",
@@ -27,7 +30,8 @@ posts ={
 		"Having a deep passion and or drive to complete something from start to finish is a common trait in many entrepreneurs. If you find yourself staying awake at night because you think you can fix something even better, or if you daydream on ways to improve something, you may be an entrepreneur without realizing it."
 		],
 	"heading":"Cloud Computing",
-	"img":"resources/img3.jpg"},
+	"img":"resources/img3.jpg",
+	"register":0},
 }
 @app.route('/')
 def home():
@@ -41,7 +45,14 @@ def article(num):
 	if num.isnumeric():
 		if(int(num)<=3):
 			articles=posts["post"+num]
-			return render_template("article.html",articles=articles)
+			return render_template("article.html",articles=articles,num=num)
+	return render_template("ecell.html")
+@app.route('/registerEvent/<num>')
+def event(num):
+	if num.isnumeric():
+		if(int(num)<=3):
+			articles=posts["post"+num]
+			return render_template("event.html",articles=articles,num=num)
 	return render_template("ecell.html")
 if __name__ == "__main__":
 	app.run(debug=True)
