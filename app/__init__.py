69df1eb0d29d30c4b48b8a635d0d455839056bdf
Vishalghyv <vishal38785@gmail.com>
2019-10-23 00:29:58 +0530

    fdf

--- app/__init__.py
+++ app/__init__.py
@@ -117,163 +117,163 @@ import json
 #	Reading from json	#
 #########################
 
-with open(os.path.abspath(os.path.join(app._static_folder, 'articles/articles.json'))) as f:
-	data = json.load(f)
-posts=data["posts"]
-headings=data["headings"]
-@app.route('/')
-def home():
-	return render_template("ecell.html")
-@app.route('/sponser')
-def sponser():
-	return render_template("sponser.html")
-@app.route('/article/<num>')
-def article(num):
-	if num.isnumeric():
-		if(int(num)<=3):
-			articles=posts["post"+num]
-			return render_template("article.html",articles=articles,headings=headings,num=num)
-	return render_template("ecell.html")
-@app.route('/registerEvent/<num>')
-def event(num):
-	if num.isnumeric():
-		if(int(num)<=3):
-			articles=posts["post"+num]
-			if(articles["register"]):
-					return render_template("event.html",articles=articles,num=num)
-	return render_template("ecell.html")
-@app.route('/listingArticle')
-def listingArticle():
-	return render_template("listing.html",posts=posts,variable=1)
-@app.route('/listingEvent')
-def listingEvent():
-	return render_template("listing.html",posts=posts,variable=0)
-@app.route('/admin')
-def admin():
-	return render_template("admin.html")
-@app.route('/adminLogin', methods=['GET', 'POST'])
-def adminLogin():
-	form = LoginForm()
-	if form.validate_on_submit():
-	    # Grab the user from our User Models table
-	    user = User.query.filter_by(email=form.email.data).first()
+# with open(os.path.abspath(os.path.join(app._static_folder, 'articles/articles.json'))) as f:
+# 	data = json.load(f)
+# posts=data["posts"]
+# headings=data["headings"]
+# @app.route('/')
+# def home():
+# 	return render_template("ecell.html")
+# @app.route('/sponser')
+# def sponser():
+# 	return render_template("sponser.html")
+# @app.route('/article/<num>')
+# def article(num):
+# 	if num.isnumeric():
+# 		if(int(num)<=3):
+# 			articles=posts["post"+num]
+# 			return render_template("article.html",articles=articles,headings=headings,num=num)
+# 	return render_template("ecell.html")
+# @app.route('/registerEvent/<num>')
+# def event(num):
+# 	if num.isnumeric():
+# 		if(int(num)<=3):
+# 			articles=posts["post"+num]
+# 			if(articles["register"]):
+# 					return render_template("event.html",articles=articles,num=num)
+# 	return render_template("ecell.html")
+# @app.route('/listingArticle')
+# def listingArticle():
+# 	return render_template("listing.html",posts=posts,variable=1)
+# @app.route('/listingEvent')
+# def listingEvent():
+# 	return render_template("listing.html",posts=posts,variable=0)
+# @app.route('/admin')
+# def admin():
+# 	return render_template("admin.html")
+# @app.route('/adminLogin', methods=['GET', 'POST'])
+# def adminLogin():
+# 	form = LoginForm()
+# 	if form.validate_on_submit():
+# 	    # Grab the user from our User Models table
+# 	    user = User.query.filter_by(email=form.email.data).first()
 
-	    # Check that the user was supplied and the password is right
-	    # The verify_password method comes from the User object
-	    # https://stackoverflow.com/questions/2209755/python-operation-vs-is-not
+# 	    # Check that the user was supplied and the password is right
+# 	    # The verify_password method comes from the User object
+# 	    # https://stackoverflow.com/questions/2209755/python-operation-vs-is-not
 
-	    if user.check_password(form.password.data) and user is not None:
-	        #Log in the user
+# 	    if user.check_password(form.password.data) and user is not None:
+# 	        #Log in the user
 
-	        login_user(user)
+# 	        login_user(user)
 
-	        # If a user was trying to visit a page that requires a login
-	        # flask saves that URL as 'next'.
-	        next = request.args.get('next')
+# 	        # If a user was trying to visit a page that requires a login
+# 	        # flask saves that URL as 'next'.
+# 	        next = request.args.get('next')
 
-	        # So let's now check if that next exists, otherwise we'll go to
-	        # the welcome page.
-	        if next == None or not next[0]=='/':
-	            next = url_for('adminPosts')
-	        return redirect(next)
-	return render_template('login.html', form=form)
-@app.route('/adminPosts')
-@login_required
-def adminPosts():
-    with open(os.path.abspath(os.path.join(app._static_folder, 'articles/articles.json'))) as f:
-        data = json.load(f)
-    posts=data["posts"]
-    headings=data["headings"]
-    return render_template('adminPosts.html',posts=posts)
+# 	        # So let's now check if that next exists, otherwise we'll go to
+# 	        # the welcome page.
+# 	        if next == None or not next[0]=='/':
+# 	            next = url_for('adminPosts')
+# 	        return redirect(next)
+# 	return render_template('login.html', form=form)
+# @app.route('/adminPosts')
+# @login_required
+# def adminPosts():
+#     with open(os.path.abspath(os.path.join(app._static_folder, 'articles/articles.json'))) as f:
+#         data = json.load(f)
+#     posts=data["posts"]
+#     headings=data["headings"]
+#     return render_template('adminPosts.html',posts=posts)
 
-@app.route('/adminRegister', methods=['GET', 'POST'])
-def adminRegister():
-    form = RegistrationForm()
+# @app.route('/adminRegister', methods=['GET', 'POST'])
+# def adminRegister():
+#     form = RegistrationForm()
 
-    if form.validate_on_submit():
-        user = User(email=form.email.data,
-                    username=form.username.data,
-                    password=form.password.data)
+#     if form.validate_on_submit():
+#         user = User(email=form.email.data,
+#                     username=form.username.data,
+#                     password=form.password.data)
 
-        db.session.add(user)
-        db.session.commit()
-        return redirect(url_for('adminLogin'))
-    return render_template('register.html', form=form)
+#         db.session.add(user)
+#         db.session.commit()
+#         return redirect(url_for('adminLogin'))
+#     return render_template('register.html', form=form)
 
-@app.route("/adminLogout")
-@login_required
-def adminLogout():
-    logout_user()
-    return redirect(url_for('admin'))
-@app.route("/adminEdit/<num>/<name>")
-@login_required
-def adminEdit(num,name):
-	post=posts[name]
-	return render_template("articleEdit.html",post=post,name=name)
-@app.route("/adminUpdate/<name>", methods=['GET', 'POST'])
-@login_required
-def adminUpdate(name):
-    blog_post = posts[name]
-    if blog_post["user_name"] != current_user.username:
-        abort(403)
+# @app.route("/adminLogout")
+# @login_required
+# def adminLogout():
+#     logout_user()
+#     return redirect(url_for('admin'))
+# @app.route("/adminEdit/<num>/<name>")
+# @login_required
+# def adminEdit(num,name):
+# 	post=posts[name]
+# 	return render_template("articleEdit.html",post=post,name=name)
+# @app.route("/adminUpdate/<name>", methods=['GET', 'POST'])
+# @login_required
+# def adminUpdate(name):
+#     blog_post = posts[name]
+#     if blog_post["user_name"] != current_user.username:
+#         abort(403)
 
-    form = BlogPostForm()
-    if form.validate_on_submit():
-        if form.picture.data:
-            pic = add_profile_pic(form.picture.data,name)
-            posts["post"+str(len(posts)-1)]["img"]="resources/"+pic
-        posts[name]["heading"] = form.title.data
-        posts[name]["article"]= form.text.data
-        if(form.event.data ==True):
-            posts[name]["register"]= 1
-        else:
-            posts[name]["register"]= 0
-        if form.picture.data:
-            pic = add_profile_pic(form.picture.data,"post"+str(len(posts)))
-            posts["post"+str(len(posts))]["img"]="resources/"+pic
-        with open(os.path.abspath(os.path.join(app._static_folder, 'articles/articles.json')), 'w') as f:
-            json.dump(data, f)
-        return redirect(url_for('adminPosts'))
-    elif request.method == 'GET':
-        form.title.data = posts[name]["heading"]
-        form.text.data = posts[name]["article"]
-        form.event.data = posts[name]["register"]
-    return render_template('create_post.html', title='Update Article',
-                           form=form)
-@app.route('/adminCreate',methods=['GET','POST'])
-@login_required
-def adminCreate():
-    form = BlogPostForm()
-    if form.validate_on_submit():
-        posts["post"+str(len(posts)+1)]={}
-        posts["post"+str(len(posts))]["heading"]=form.title.data
-        posts["post"+str(len(posts))]["num"]=len(posts)
-        headings["heading"+str(len(headings)+1)]=form.title.data
-        posts["post"+str(len(posts))]["article"]=form.text.data
-        posts["post"+str(len(posts))]["user_name"]=current_user.username
-        if(form.event.data ==True):
-            posts["post"+str(len(posts))]["register"]= 1
-        else:
-            posts["post"+str(len(posts))]["register"]= 0
-        if form.picture.data:
-            pic = add_profile_pic(form.picture.data,"post"+str(len(posts)))
-            posts["post"+str(len(posts))]["img"]="resources/"+pic
-        with open(os.path.abspath(os.path.join(app._static_folder, 'articles/articles.json')), 'w') as f:
-            json.dump(data, f)
-        return redirect(url_for('adminPosts'))
-    return render_template('create_post.html',form=form)
-@app.route("/adminDelete/<name>", methods=['POST'])
-@login_required
-def adminDelete(name):
-    blog_post = posts[name]
-    if blog_post["user_name"] != current_user.username:
-        abort(403)
-    del posts[name]
-    with open(os.path.abspath(os.path.join(app._static_folder, 'articles/articles.json')), 'w') as f:
-            json.dump(data, f)
-    return redirect(url_for('adminPosts'))
-@app.route('/<path:path>')
-def catch_all(path):
-    return render_template("ecell.html")
+#     form = BlogPostForm()
+#     if form.validate_on_submit():
+#         if form.picture.data:
+#             pic = add_profile_pic(form.picture.data,name)
+#             posts["post"+str(len(posts)-1)]["img"]="resources/"+pic
+#         posts[name]["heading"] = form.title.data
+#         posts[name]["article"]= form.text.data
+#         if(form.event.data ==True):
+#             posts[name]["register"]= 1
+#         else:
+#             posts[name]["register"]= 0
+#         if form.picture.data:
+#             pic = add_profile_pic(form.picture.data,"post"+str(len(posts)))
+#             posts["post"+str(len(posts))]["img"]="resources/"+pic
+#         with open(os.path.abspath(os.path.join(app._static_folder, 'articles/articles.json')), 'w') as f:
+#             json.dump(data, f)
+#         return redirect(url_for('adminPosts'))
+#     elif request.method == 'GET':
+#         form.title.data = posts[name]["heading"]
+#         form.text.data = posts[name]["article"]
+#         form.event.data = posts[name]["register"]
+#     return render_template('create_post.html', title='Update Article',
+#                            form=form)
+# @app.route('/adminCreate',methods=['GET','POST'])
+# @login_required
+# def adminCreate():
+#     form = BlogPostForm()
+#     if form.validate_on_submit():
+#         posts["post"+str(len(posts)+1)]={}
+#         posts["post"+str(len(posts))]["heading"]=form.title.data
+#         posts["post"+str(len(posts))]["num"]=len(posts)
+#         headings["heading"+str(len(headings)+1)]=form.title.data
+#         posts["post"+str(len(posts))]["article"]=form.text.data
+#         posts["post"+str(len(posts))]["user_name"]=current_user.username
+#         if(form.event.data ==True):
+#             posts["post"+str(len(posts))]["register"]= 1
+#         else:
+#             posts["post"+str(len(posts))]["register"]= 0
+#         if form.picture.data:
+#             pic = add_profile_pic(form.picture.data,"post"+str(len(posts)))
+#             posts["post"+str(len(posts))]["img"]="resources/"+pic
+#         with open(os.path.abspath(os.path.join(app._static_folder, 'articles/articles.json')), 'w') as f:
+#             json.dump(data, f)
+#         return redirect(url_for('adminPosts'))
+#     return render_template('create_post.html',form=form)
+# @app.route("/adminDelete/<name>", methods=['POST'])
+# @login_required
+# def adminDelete(name):
+#     blog_post = posts[name]
+#     if blog_post["user_name"] != current_user.username:
+#         abort(403)
+#     del posts[name]
+#     with open(os.path.abspath(os.path.join(app._static_folder, 'articles/articles.json')), 'w') as f:
+#             json.dump(data, f)
+#     return redirect(url_for('adminPosts'))
+# @app.route('/<path:path>')
+# def catch_all(path):
+#     return render_template("ecell.html")
 if __name__ == "__main__":
 	app.run(debug=False)
